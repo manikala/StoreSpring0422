d9f972315cf8e930033be06e4bba7ecfd6543290
Valeriy <manikala777@rambler.ru>
2022-04-14 14:51:57 +0300

    Create bucket

--- v1/src/main/java.store.spring/obj/Bucket.java
+++ v1/src/main/java.store.spring/obj/Bucket.java
@@ -1,4 +1,11 @@
 package obj;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bucket {
+    private int id = 0;
+    private String user;
+    private double totalAmount;
+    List<String> products = new ArrayList<>();
 }
