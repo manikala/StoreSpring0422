5749110ef3a07104a3323db0a0b00bc0b32c2e54
Valeriy <manikala777@rambler.ru>
2022-04-15 23:06:43 +0300

    add method in bucket

--- v1/src/main/java.store.spring/obj/Bucket.java
+++ v1/src/main/java.store.spring/obj/Bucket.java
@@ -6,6 +6,15 @@ import java.util.List;
 public class Bucket {
     private int id = 0;
     private String user;
-    private double totalAmount;
-    List<String> products = new ArrayList<>();
+    private double totalAmount = 0;
+    List<Product> products = new ArrayList<>();
+
+    public Bucket(String user) {
+        this.user = user;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
 }
