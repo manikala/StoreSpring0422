3e1912ea759354d909669675226695aa2add87e0
Valeriy <manikala777@rambler.ru>
2022-04-25 00:03:51 +0300

    add my user sql

---
+++ v1/src/main/resources/db/db_add_user.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id, archive, username, lastname, firstname, password, bucket_id)
+VALUES (1, false, 'manikala', 'valera', 'manikala', 'manikala', null);
+
+ALTER SEQUENCE user_seq RESTART WITH 2;
